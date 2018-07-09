23b204b1ec7bff5fb2b86ab097a5254d9d1d9d6d
alendri <alendri@allbin>
2018-07-09 13:36:16 +0200

    Setup build and release process.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,25 @@
 let gulp = require('gulp4');
 let allbin = require('gulp-allbin');
 
+let sourcemaps = require('gulp-sourcemaps');
+let ts = require('gulp-typescript');
+let babel = require('gulp-babel');
+let tsProject = ts.createProject('./tsconfig.json');
+
 
 gulp.task('release', allbin.buildBumpPatchPush());
 gulp.task('release:minor', allbin.buildBumpMinorPush());
 gulp.task('release:major', allbin.buildBumpMajorPush());
+
+
+
+gulp.task('releasets', function() {
+    return gulp.src(['src/**/*.ts', 'src/**/*.js'])
+        .pipe(sourcemaps.init())
+        .pipe(tsProject())
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'));
+});
